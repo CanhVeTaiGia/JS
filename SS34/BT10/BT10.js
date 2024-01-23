29473094d622c950516d1cc2c1c4a0410bdada88
CanhVeTaiGia <147359294+CanhVeTaiGia@users.noreply.github.com>
2024-01-23 15:56:22 +0700

    LHMV

---
+++ SS34/BT10/BT10.js
@@ -0,0 +1,6 @@
+// innerText: Là một thuộc tính dành cho trình duyệt IE 6. Nó lấy nội dung bên trong của phần tử và bỏ qua các phần tử không hiển thị nội dung.
+// textContent: Là một thuộc tính W3C chuẩn hóa, tương tự như innerText. Nó lấy nội dung bên trong của phần tử, nhưng không bỏ qua các phần tử không hiển thị nội dung.
+// innerHTML: Là một thuộc tính dành cho trình duyệt IE 5.5, nó lấy nội dung bên trong của phần tử, bao gồm cả các thẻ HTML và nội dung của chúng.
+// innerText không lấy nội dung của các phần tử ẩn (display: none).
+// textContent lấy nội dung của các phần tử ẩn.
+// innerHTML lấy nội dung bao gồm các thẻ HTML.
